1e841ace3335b6f6a1dea8fecf73d4ef2569cd1e
G285212022 <228521@st.takushoku-u.ac.jp>
2023-07-20 19:40:42 +0900

    6

--- project.js
+++ project.js
@@ -4,15 +4,34 @@ b.addEventListener('click', greeting);
 function greeting() {
 	let i = document.querySelector('input[name="id"]');
 	let id = i.value;
-	console.log(id);
-  let a = document.querySelector('script#sid'); 
-  let c = document.createElement('src'); 
-  a.textContent = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + id + '.json';
-  c.insertAdjacentElement('afterend', a);
-  let d = document.querySelector('div#result'); 
-  let e = document.createElement('p'); 
-  d.textContent = data;
-  e.insertAdjacentElement('afterend', d);
+	console.log(id); 
+    let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + id + '.json';
+
+    axios.get(url)
+        .then(showResult)   
+        .catch(showError)   
+        .then(finish);
+}
+
+function showResult(resp) {
+    let data = resp.data;
+
+    if(typeof data ==='string') {
+        data = JSON.parse(data);
+    }
+    console.log(data);
+    let h1 = document.querySelector('div#result'); 
+    let p1 = document.createElement('p'); 
+    p1.textContent = data.coord.lon;
+    h1.insertAdjacentElement('beforeend', p1);
+}
+
+function showError(err){
+    console.log(err);
+}
+
+function finish() {
+    console.log(finish);
 }
 
 
